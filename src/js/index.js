314ba40986ef47d0bafb0dddae057e03d9e39f0e
chandanomnie <chandansingh@omniesolutions.com>
2024-08-28 18:28:56 +0530

    About Us

--- src/js/index.js
+++ src/js/index.js
@@ -107,7 +107,6 @@ jQuery(function ($) {
     });
   }
 
-
   if ($(".about-life-slider").length) {
     let SwiperAbout = new Swiper(".about-life-slider", {
       spaceBetween: 0,
@@ -122,8 +121,8 @@ jQuery(function ($) {
       //   pauseOnMouseEnter: true,
       // },
       navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
       },
       loop: true,
       slidesPerView: "auto",
@@ -158,9 +157,7 @@ jQuery(function ($) {
     });
   }
 
-
   scrollContent();
-
 });
 
 $(window).on("load", function () {
@@ -179,7 +176,6 @@ $(window).on("load", function () {
   }
 });
 
-
 var browserMobile = false;
 if (jQuery("body").hasClass("layout-mobile")) browserMobile = true;
 jQuery(window).scroll(function () {
@@ -188,8 +184,6 @@ jQuery(window).scroll(function () {
 });
 
 function scrollContent() {
-
-
   if (jQuery(".count-container .animCounter").length) {
     // var aboutCounterUp;
     jQuery(".count-container .animCounter").each(function () {
@@ -203,7 +197,7 @@ function scrollContent() {
         countTo: getTargetVal,
         placeholder: 0,
         easing: "swing",
-        onStart: function () { },
+        onStart: function () {},
         onComplete: function () {
           jQuery(".count-container .animCounter").addClass("completed");
         },
@@ -322,13 +316,12 @@ function scrollContent() {
 
         jQuery(".count-container .animCounter").each(function () {
           if (
-            jQuery(".count-container .animCounter").hasClass("completed") !== true
+            jQuery(".count-container .animCounter").hasClass("completed") !==
+            true
           ) {
             jQuery(this).counter("start");
           }
         });
-
-
 
         if (
           object.find(".animCounter").hasClass("aboutCounter") &&
@@ -388,3 +381,59 @@ function whichBrs() {
     return navigator.userAgent.substr(0, agt.indexOf(" "));
   else return navigator.userAgent;
 }
+
+var words = document.getElementsByClassName("word");
+var wordArray = [];
+var currentWord = 0;
+
+words[currentWord].style.opacity = 1;
+for (var i = 0; i < words.length; i++) {
+  splitLetters(words[i]);
+}
+
+function changeWord() {
+  var cw = wordArray[currentWord];
+  var nw =
+    currentWord == words.length - 1 ? wordArray[0] : wordArray[currentWord + 1];
+  for (var i = 0; i < cw.length; i++) {
+    animateLetterOut(cw, i);
+  }
+
+  for (var i = 0; i < nw.length; i++) {
+    nw[i].className = "letter behind";
+    nw[0].parentElement.style.opacity = 1;
+    animateLetterIn(nw, i);
+  }
+
+  currentWord = currentWord == wordArray.length - 1 ? 0 : currentWord + 1;
+}
+
+function animateLetterOut(cw, i) {
+  setTimeout(function () {
+    cw[i].className = "letter out";
+  }, i * 80);
+}
+
+function animateLetterIn(nw, i) {
+  setTimeout(function () {
+    nw[i].className = "letter in";
+  }, 340 + i * 80);
+}
+
+function splitLetters(word) {
+  var content = word.innerHTML;
+  word.innerHTML = "";
+  var letters = [];
+  for (var i = 0; i < content.length; i++) {
+    var letter = document.createElement("span");
+    letter.className = "letter";
+    letter.innerHTML = content.charAt(i);
+    word.appendChild(letter);
+    letters.push(letter);
+  }
+
+  wordArray.push(letters);
+}
+
+changeWord();
+setInterval(changeWord, 4000);
