72bf28d069fe206e8d91e23d6cc73fb42e309eaf
Mahendra Singh Rana <Mahendra@om.omnieserver.com>
2024-08-28 20:10:25 +0530

    mahendra  28  aug   24

--- src/js/index.js
+++ src/js/index.js
@@ -157,6 +157,23 @@ jQuery(function ($) {
     });
   }
 
+  if (jQuery("[data-splitting]").length) {
+
+    Splitting({
+      /* target: String selector, Element, Array of Elements, or NodeList */
+      target: "[data-splitting]",
+      /* by: String of the plugin name */
+      by: "chars",
+      /* key: Optional String to prefix the CSS variables */
+      key: null
+
+    });
+
+
+  }
+
+
+
   scrollContent();
 });
 
@@ -197,7 +214,7 @@ function scrollContent() {
         countTo: getTargetVal,
         placeholder: 0,
         easing: "swing",
-        onStart: function () {},
+        onStart: function () { },
         onComplete: function () {
           jQuery(".count-container .animCounter").addClass("completed");
         },
