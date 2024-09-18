2b7a049c3749cdb134331143beda9f83356fe719
chandanomnie <chandansingh@omniesolutions.com>
2024-09-18 19:30:51 +0530

    Service page

--- src/js/index.js
+++ src/js/index.js
@@ -817,3 +817,20 @@ if (jQuery(".word").length) {
   changeWord();
   setInterval(changeWord, 4000);
 }
+
+const scrollers = document.querySelectorAll(".scroller");
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
+
+addAnimation();
